f1efaec376fcd4adc37ee1338e6f22e290ace9e7
Kelly Campbell <kelly.a.campbell@gmail.com>
2015-11-09 12:36:42 -0500

    First pass at a BLE plugin TypeScript definition.
    Doesn't include the promise changes. Also not really complete
    on types of params.

--- www/ble.d.ts
+++ www/ble.d.ts
@@ -1,27 +1,28 @@
 interface BluetoothLowEnergy {
 
   // TODO(kellyc): try to close in the any types
-  scan(services: any, seconds: number, success: (data: any) => any, failure: (err: any) => any): void;  
-  startScan(services: any, success: () => any, failure: () => any): void;
-  stopScan(services: any, success: () => any, failure: () => any): void;
-  list(success: () => any, failure: () => any): void;
-  connect(device_id: any, success: () => any, failure: () => any): void;
-  disconnect(device_id: any, success: () => any, failure: () => any): void;
-  read(device_id: any, service_uuid: string, characteristic_uuid: string, success: () => any, failure: () => any): void;
-  write(device_id: any, service_uuid: string, characteristic_uuid: string, value: any, success: () => any, failure: () => any): void;
-  writeWithoutRepsonse(device_id: any, service_uuid: string, characteristic_uuid: string, value: any, success: () => any, failure: () => any): void;
-  writeCommand(device_id: any, service_uuid: string, characteristic_uuid: string, value: any, success: () => any, failure: () => any): void;
-  notify(device_id: any, service_uuid: string, characteristic_uuid: string, success: () => any, failure: () => any): void;
-  startNotification(device_id: any, service_uuid: string, characteristic_uuid: string, success: () => any, failure: () => any): void;
-  stopNotification(device_id: any, service_uuid: string, characteristic_uuid: string, success: () => any, failure: () => any): void;
-  isConnected(device_id: any, success: () => any, failure: () => any): void;
-  isEnabled(success: () => any, failure: () => any): void;
-  enable(success: () => any, failure: () => any): void;
-  showBluetoothSettings(success: () => any, failure: () => any): void;
+  scan(services: any, seconds: number, success: (result: any) => any, failure: (error: any) => any): void;  
+  startScan(services: any, success: (result: any) => any, failure: (error: any) => any): void;
+  stopScan(services: any, success: (result: any) => any, failure: (error: any) => any): void;
+  list(success: (result: any) => any, failure: (error: any) => any): void;
+  connect(device_id: any, success: (result: any) => any, failure: (error: any) => any): void;
+  disconnect(device_id: any, success: (result: any) => any, failure: (error: any) => any): void;
+  read(device_id: any, service_uuid: string, characteristic_uuid: string, success: (result: any) => any, failure: (error: any) => any): void;
+  write(device_id: any, service_uuid: string, characteristic_uuid: string, value: any, success: (result: any) => any, failure: (error: any) => any): void;
+  writeWithoutRepsonse(device_id: any, service_uuid: string, characteristic_uuid: string, value: any, success: (result: any) => any, failure: (error: any) => any): void;
+  writeCommand(device_id: any, service_uuid: string, characteristic_uuid: string, value: any, success: (result: any) => any, failure: (error: any) => any): void;
+  notify(device_id: any, service_uuid: string, characteristic_uuid: string, success: (result: any) => any, failure: (error: any) => any): void;
+  startNotification(device_id: any, service_uuid: string, characteristic_uuid: string, success: (result: any) => any, failure: (error: any) => any): void;
+  stopNotification(device_id: any, service_uuid: string, characteristic_uuid: string, success: (result: any) => any, failure: (error: any) => any): void;
+  isConnected(device_id: any, success: (result: any) => any, failure: (error: any) => any): void;
+  isEnabled(success: (result: any) => any, failure: (error: any) => any): void;
+  enable(success: (result: any) => any, failure: (error: any) => any): void;
+  showBluetoothSettings(success: (result: any) => any, failure: (error: any) => any): void;
 }
+
+declare var ble: BluetoothLowEnergy;
 
 interface Window {
   ble: BluetoothLowEnergy
 }
 
-declare var ble: BluetoothLowEnergy;
